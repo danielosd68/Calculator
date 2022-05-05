424e436377403390d29d8a11f9e1e6c019ae3179
Daniel <danielosd68@gmail.com>
2022-05-05 15:44:30 +0200

    -

--- scripts/animations.js
+++ scripts/animations.js
@@ -6,31 +6,13 @@ jQuery(document).ready(() => {
     $("h2").animate({ left: "0%" }, 1000);
     setTimeout(() => { $("h2").removeClass("disabled-header"); }, 1000);
 
-    setTimeout(() => {
-        $("li:nth-child(1)").animate({ left: "0%" }, 1000);
-    }, 1200);
+    for (let i = 0; i < $("li").length; i++) {
 
-    setTimeout(() => {
-        $("li:nth-child(2)").animate({ left: "0%" }, 1000);
-    }, 1400);
+        setTimeout(() => {
 
-    setTimeout(() => {
-        $("li:nth-child(3)").animate({ left: "0%" }, 1000);
-    }, 1600);
+            $(`li:nth-child(${i + 1})`).animate({ left: "0%" }, 1000);
+            setTimeout(() => { $(`li:nth-child(${i + i})`).removeClass("disabled-header"); }, 1000);
+        }, 1000 + (i * 100));
+    }
 
-    setTimeout(() => {
-        $("li:nth-child(4)").animate({ left: "0%" }, 1000);
-    }, 1800);
-
-    setTimeout(() => {
-        $("li:nth-child(5)").animate({ left: "0%" }, 1000);
-    }, 2000);
-
-    setTimeout(() => {
-        $("li:nth-child(6)").animate({ left: "0%" }, 1000);
-    }, 2200);
-
-    setTimeout(() => {
-        $("li:nth-child(7)").animate({ left: "0%" }, 1000);
-    }, 2400);
 });
