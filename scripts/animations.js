177018e0eeaab20d751151544366c2f5f6beadd3
Daniel <danielosd68@gmail.com>
2022-05-05 15:50:29 +0200

    -

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,17 +1,13 @@
-$("h2").addClass("disabled-header");
-$("li").addClass("disabled-li");
 
 jQuery(document).ready(() => {
 
     $("h2").animate({ left: "0%" }, 1000);
-    setTimeout(() => { $("h2").removeClass("disabled-header"); }, 1000);
+
 
     for (let i = 0; i < $("li").length; i++) {
 
         setTimeout(() => {
-
             $(`li:nth-child(${i + 1})`).animate({ left: "0%" }, 1000);
-            setTimeout(() => { $(`li:nth-child(${i + i})`).removeClass("disabled-header"); }, 1000);
         }, 1000 + (i * 100));
     }
 
